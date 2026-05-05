25ffcebd6dca23c35864539a1a73f4943793bc8f
Tucker <151984953+TuckerPMIZ@users.noreply.github.com>
2026-05-05 14:01:41 -0500

    Upload current files worked on
    I'm still working on these. So far, I believe EnumLabel.java, PackageType.java, and RealmType.java are done. The others are close. I'll have to upload DeliveryCostStrategy along with ExpressDeliveryCostStrategy and CarefulDeliveryCostStrategy later.

---
+++ EnumLabel.java
@@ -0,0 +1,12 @@
+package edu.oop.guild.model;
+
+/*	File: EnumLabel.java
+ * 	Author: Tucker Potts
+ *  Description: This file...
+ * 
+ * 
+ */
+public interface EnumLabel {
+	public String displayName();
+	public String label();
+}
